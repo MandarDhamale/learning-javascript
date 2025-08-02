98e9e55c0d08cb5ae0811305bcaa2a52359a6cc5
Mandar Dhamale <mandardhamale@gmail.com>
2025-08-02 07:43:09 +0530

    day log

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-alert('Alert');
-console.log('log');
+// alert('Alert');
+// console.log('log');
+
+const names = ['mandar', 'makarand', 'manik', 'vijaya'];
+
+for(let i = 0; i<names.length; i++){
+    console.log(names[i]);
+}
