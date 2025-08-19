72205917750a18e06a6247c3b04ffbcc6f1a76a9
Mandar Dhamale <mandardhamale@gmail.com>
2025-08-18 22:39:13 -0400

    new albums-ui

---
+++ React/albums-ui/src/menu-items/index.jsx
@@ -0,0 +1,11 @@
+// project import
+import pages from './page';
+import support from './support';
+
+// ==============================|| MENU ITEMS ||============================== //
+
+const menuItems = {
+  items: [pages, support]
+};
+
+export default menuItems;
---
+++ React/albums-ui/src/routes/MainRoutes.jsx
@@ -0,0 +1,23 @@
+import { lazy } from 'react';
+
+// project imports
+import Loadable from 'components/Loadable';
+import DashboardLayout from 'layout/Dashboard';
+
+// render - sample page
+const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
+
+// ==============================|| MAIN ROUTING ||============================== //
+
+const MainRoutes = {
+  path: '/',
+  element: <DashboardLayout />,
+  children: [
+    {
+      path: '/',
+      element: <SamplePage />
+    },
+      ]
+};
+
+export default MainRoutes;
