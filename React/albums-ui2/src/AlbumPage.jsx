a842e14bc1c268a88ddec6ea2ec99e13b1ad8a17
Mandar Dhamale <mandardhamale@gmail.com>
2025-10-13 00:13:11 -0400

    feat: photo page update

--- React/albums-ui2/src/AlbumPage.jsx
+++ React/albums-ui2/src/AlbumPage.jsx
@@ -2,51 +2,58 @@ import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 function AlbumPage() {
-    const {albumId} = useParams();
+  const { albumId } = useParams();
 
-    const [album, setAlbum] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [album, setAlbum] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        const fetchAlbum = async () => {
-            try{
-                const authToken = localStorage.getItem('authToken');
-                const response = await fetch(`http://localhost:8080/api/v1/album/albums/${albumId}`, {
-                    headers: {
-                        Authorization: `Bearer ${authToken}`
-                    }
-                })
+  useEffect(() => {
+    const fetchAlbum = async () => {
+      try {
+        const authToken = localStorage.getItem("authToken");
+        const response = await fetch(
+          `http://localhost:8080/api/v1/album/albums/${albumId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${authToken}`,
+            },
+          }
+        );
 
-                if(!response.ok){
-                    throw new Error("Failed to get the album photos :(");
-                }
-
-                const data = await response.json();
-                console.log(data);
-                setAlbum(data);
-
-            }catch(error){
-                setError(error.message);
-            }finally{
-                setLoading(false);
-            }
+        if (!response.ok) {
+          throw new Error("Failed to get the album photos :(");
         }
-        fetchAlbum();
-    }, []);
 
-    if(loading){
-        return <div>Loading...</div>
-    }
-    if(error){
-        return <div>Error: {error}</div>
-    }
+        const data = await response.json();
+        console.log(data);
+        setAlbum(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchAlbum();
+  }, []);
 
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
-    return (
-        <div>Album Page (ID:{albumId})</div>
-        
-    )
+  if (album && album.photos && album.photos.length === 0) {
+    return <div>No photos added</div>;
+  }
+
+  // If the album data itself wasn't found
+  if (!album) {
+    return <div>Album not found</div>;
+  }
+
+  return <div>Album Page (ID:{albumId})</div>;
 }
 
 export default AlbumPage;
